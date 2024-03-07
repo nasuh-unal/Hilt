03e5da7825492f5ac8735693ab455729b889fc00
Nasuh Unal <nasuhunal1@gmail.com>
2024-03-07 13:37:32 +0300

    first-commit

---
+++ app/src/main/java/com/example/android/hilt/data/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.example.android.hilt.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Log::class), version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun logDao(): LogDao
+}
